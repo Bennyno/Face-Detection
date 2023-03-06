856124de69d2339501d465cd68876c1cbdf46645
Bennyno <ben.fernandez28@gmail.com>
2023-03-06 18:47:19 -0500

    changed url back

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
   } 
 
   onSubmitSignIn =( ) =>{
-    fetch('https://elegant-narwhal-1f262a.netlify.app/signin', {
+    fetch('https://face-detection-backend.herokuapp.com/signin', {
       method: 'post',
       headers: {'content-type': 'application/json'},
       body: JSON.stringify({
